c7ab5c07f04e89fc7cd3b294a99d42d47e72863e
Jaremy Creechley <creechley@gmail.com>
2019-10-05 14:50:07 -0600

    initial commit

--- lib/pidex.ex
+++ lib/pidex.ex
@@ -1,18 +1,37 @@
+defmodule Pidex.State do
+  defstruct error: 0.0, integral: 0.0, bias: 0.0, ts: 0
+end
+
 defmodule Pidex do
   @moduledoc """
   Documentation for Pidex.
   """
+  defstruct set_point: 0.0, min_point: 0.0, max_point: 0.0, kP: 0.0, kI: 0.0, kD: 0.0
 
-  @doc """
-  Hello world.
+  def update({%Pidex{} = pid, %Pidex.State{} = state, process_value, ts}) do
+   update(pid, state, process_value, ts)
+  end
 
-  ## Examples
+  def update(%Pidex{kP: kP, kI: kI, kD: kD, set_point: target} = pid, %Pidex.State{} = state, process_value, ts) do
 
-      iex> Pidex.hello()
-      :world
+    dT = ts - state.ts
+    p_error! = target - process_value
+    p_integral! = state.integral + (p_error! * dT)
+    p_derivative! = (p_error! - state.error) / dT
 
-  """
-  def hello do
-    :world
+    output = kP*p_error! + kI*p_integral! + kD*p_derivative! + state.bias
+
+    %{min_point: min_point, max_point: max_point} = pid
+    output =
+      case output do
+        updated_value when updated_value < min_point ->
+          pid.min_point
+        updated_value when updated_value > max_point ->
+          max_point
+        updated_value ->
+          updated_value
+      end
+
+    {output, %Pidex.State{error: p_error!, integral: p_integral!}}
   end
 end
